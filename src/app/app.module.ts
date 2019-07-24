224617b1e1208ff6362c833876ae87e119baa946
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-07-24 18:29:28 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { AptitudeComponent } from './aptitude/aptitude.component';
-=======
 import { DetailsComponent } from './details/details.component';
->>>>>>> origin/emma
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     AptitudeComponent
-=======
     DetailsComponent
->>>>>>> origin/emma
   ],
   imports: [
     BrowserModule
